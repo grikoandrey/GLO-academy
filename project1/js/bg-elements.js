f2f7d39aa0accf89c88bdb42a00bfd8c1c53e721
grikoandrey <griko.and@gmail.com>
2024-10-22 19:11:36 +0200

    lesson1 anime

---
+++ project1/js/bg-elements.js
@@ -0,0 +1,11 @@
+let elements = document.querySelectorAll('.set-bg');
+
+// console.log(elements);
+for (let i = 0; i < elements.length; i++) {
+    // console.dir(elements[i]);
+    let src = elements[i].dataset.setbg;
+
+    elements[i].style.backgroundImage = `url(${src})`;
+    // console.log(src);
+    // console.dir(elements[i]);
+}
---
+++ project1/js/preloader.js
@@ -0,0 +1,10 @@
+// console.log(document.querySelector('.preloder'));
+
+let preloader = document.querySelector('.preloder');
+
+// console.log(preloader);
+preloader.classList.add('active');
+
+setTimeout(() => {
+    preloader.classList.remove('active');
+}, 500);
