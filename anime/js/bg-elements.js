209adf13af29ecff90934e787a782541d3225427
grikoandrey <griko.and@gmail.com>
2024-10-23 14:42:48 +0200

    lesson3 anime

--- anime/js/bg-elements.js
+++ anime/js/bg-elements.js
@@ -1,15 +1,40 @@
 const bgElements = () => {
     let elements = document.querySelectorAll('.set-bg');
 
+    elements.forEach((elem, index, array) => {
+        // let src = elem.dataset.setbg;
+        elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+    });
+
     // console.log(elements);
-    for (let i = 0; i < elements.length; i++) {
-        // console.dir(elements[i]);
-        let src = elements[i].dataset.setbg;
+    // for (let i = 0; i < elements.length; i++) {
+    //     // console.dir(elements[i]);
+    //     let src = elements[i].dataset.setbg;
 
-        elements[i].style.backgroundImage = `url(${src})`;
-        // console.log(src);
-        // console.dir(elements[i]);
-    }
+    //     elements[i].style.backgroundImage = `url(${src})`;
+    //     // console.log(src);
+    //     // console.dir(elements[i]);
+    // }
+
+    //это пробный код для понимания работы методов с массивом
+    // let array = [5, 3, 2, 4, 1];
+    // let array2 = [
+    //     {
+    //         id: 0,
+    //         value: 100
+    //     },
+    //     {
+    //         id: 2,
+    //         value: 300
+    //     },
+    //     {
+    //         id: 1,
+    //         value: 200
+    //     },
+    // ]
+    // let newArray = array.filter((item) => item <= 3);
+    // let new2Array = array2.sort((a, b) => a.value - b.value);
+    // console.log(newArray);
+    // console.log(new2Array);
 }
-
 bgElements();
--- anime/js/mainData.js
+++ anime/js/mainData.js
@@ -1,11 +1,47 @@
 const mainData = () => {
+
+    let renderAnimeList = (array, ganres) => {
+        console.log(array);
+        console.log(ganres);
+    };
+
+    let renderTopAnime = (array) => {
+        let wrapper = document.querySelector('.filter__gallery');
+        // console.log(array)
+        wrapper.innerHTML = '';
+        array.forEach(el => {
+            // console.log(element);
+            wrapper.insertAdjacentHTML('afterbegin', `
+                <div class="product__sidebar__view__item set-bg mix" data-setbg="${el.image}">
+                    <div class="ep">${el.rating} / 10</div>
+                    <div class="view"><i class="fa fa-eye"></i> ${el.views}</div>
+                    <h5><a href="/anime-details.html">${el.title}</a></h5>
+                </div>
+                `)
+        });
+        // console.log(wrapper);
+        wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+            elem.style.backgroundImage = `url(${elem.dataset.setbg})`;
+        });
+    }
+
+
     fetch('https://anime-718a5-default-rtdb.europe-west1.firebasedatabase.app/anime.json')
+        // fetch('./db-2.json')
         .then((response) => {
             // console.log(response);
             return response.json();
         })
         .then((data) => {
-            console.log(data);
+            let ganres = new Set();
+
+            renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+
+            data.forEach((item) => {
+                ganres.add(item.ganre);
+            })
+            // console.log(ganres);
+            renderAnimeList(data, ganres);
         });
 }
 mainData();
