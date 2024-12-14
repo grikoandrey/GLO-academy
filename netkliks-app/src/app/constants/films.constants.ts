8b85ebefe12c3ebcacb394f2baf572eeb614b5e5
grikoandrey <griko.and@gmail.com>
2024-12-14 22:05:34 +0100

    tune angular lesson4

---
+++ netkliks-app/src/app/constants/films.constants.ts
@@ -0,0 +1,58 @@
+import { FilmType } from "../models/films.model";
+
+export const FILMS: FilmType[] = [
+    {
+        id: 0,
+        title: 'Марсианин 1',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: '/film.html',
+        image: 'images/film.png'
+    },
+    {
+        id: 1,
+        title: 'Марсианин 2',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: '/film.html',
+        image: 'images/film.png'
+    },
+    {
+        id: 2,
+        title: 'Марсианин 3',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: '/film.html',
+        image: 'images/film.png'
+    },
+    {
+        id: 3,
+        title: 'Марсианин 4',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: '/film.html',
+        image: 'images/film.png'
+    },
+    {
+        id: 4,
+        title: 'Марсианин 5',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: '/film.html',
+        image: 'images/film.png'
+    },
+    {
+        id: 5,
+        title: 'Марсианин 6',
+        original: 'The Martian 2015',
+        category: 'Фантастика, приключения',
+        rating: 7.8,
+        link: '/film.html',
+        image: 'images/film.png'
+    },
+]
---
+++ netkliks-app/src/app/models/films.model.ts
@@ -0,0 +1,9 @@
+export interface FilmType {
+    id: number;
+    title: string;
+    original: string;
+    category: string;
+    rating: number;
+    link: string;
+    image: string;
+}
